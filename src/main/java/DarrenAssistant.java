eaa26ed0bb79dae5707294b1c50ed77c237578b0
darrensimmx <darrensimmx@gmail.com>
2025-08-22 01:00:35 +0800

    Completed Level1

--- src/main/java/DarrenAssistant.java
+++ src/main/java/DarrenAssistant.java
@@ -1,10 +1,55 @@
+import java.util.Scanner;
+
 public class DarrenAssistant {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello! I'm DarrenAssistant, Darren's Personal Assistant Chatbot. What can I do for you?\n" + logo);
+        new Duke().run();
+    }
+}
+
+class Duke {
+    private final Scanner sc;
+
+    //Constructor
+    public Duke() {
+        sc = new Scanner(System.in);
+    }
+
+    public void run() {
+        Ui.printWelcome();
+
+        while (true) {
+            String input = sc.nextLine();
+
+            if (input.toLowerCase().equals("bye")) {
+                Ui.printGoodbye();
+                break;
+            }
+
+            Ui.printEcho(input);
+        }
+    }
+}
+
+class Ui {
+    public static void printWelcome() {
+        printLine();
+        System.out.println("Hello! Im DarrenAssistant, the finest chatbot you will find out there!\n What can I do for you?");
+        printLine();
+    }
+
+    public static void printEcho(String input) {
+        printLine();
+        System.out.println(input);
+        printLine();
+    }
+
+    public static void printGoodbye() {
+        printLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        printLine();
+    }
+
+    private static void printLine() {
+        System.out.println("_____________________________________________________");
     }
 }
